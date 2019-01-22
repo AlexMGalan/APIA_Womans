413b1e5fbb48062ce9b345f596ae00bd3db0f759
SMARTSALARY\Alex.Galan <alex.galan@smartsalary.com.au>
2019-01-23 10:50:35 +1100

    Firebase news fragment working

--- app/src/main/java/com/acacias/altfc/NewsFragment.java
+++ app/src/main/java/com/acacias/altfc/NewsFragment.java
@@ -11,15 +11,12 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -27,47 +24,66 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-
 public class NewsFragment extends Fragment {
 
-    private ListView mListView;
+        private ListView listView;
+        private FirebaseDatabase database;
+        private DatabaseReference myRef;
+        private ArrayList<String> mNews = new ArrayList<>();
 
-public NewsFragment() {
+
+    public NewsFragment() {
         // Required empty public constructor
-        }
+    }
 
-@Override
-public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_news, container, false);
 
         getActivity().setTitle("News");
 
-
-        // Find the ListView
-        mListView = (ListView) v.findViewById(R.id.listViewNews);
-
-        /*
-         * Create a DatabaseReference to the data; works with standard DatabaseReference methods
-         * like limitToLast() and etc.
-         */
-        DatabaseReference newsReference = FirebaseDatabase.getInstance().getReference()
-                .child("News");
-
-        // Now set the adapter with a given layout
-    //    mListView.setAdapter(new FirebaseListAdapter<News>(getActivity(), News.class,
-      //          android.R.layout.simple_list_item_1, newsReference) {
-
-            // Populate view as needed
-       //     @Override
-       //     protected void populateView(View view, News news, int position) {
-        //        ((TextView) view.findViewById(android.R.id.text1)).setText(news.getNews());
-       //     }
-      //  });
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("News_Title");
 
 
-            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      //  ListView myList = (ListView) v.findViewById(R.id.listViewNews);
+
+        listView = (ListView) v.findViewById(R.id.listViewNews);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mNews);
+        listView.setAdapter(arrayAdapter);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mNews.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
@@ -79,8 +95,7 @@ public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 fragment2.setArguments(args);
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.mainLayout, fragment2);
-                fragmentTransaction.commit();
-            }
+                fragmentTransaction.commit();            }
         });
 
         return v;
